a9618535f9b88504371cf7b49ee8e7bbd92875fa
renx005 <renaelzx@gmail.com>
2026-03-04 01:13:59 +0530

    Deployment ready

---
+++ tasks/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from .models import Task
+
+def task_list(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        if title:
+            Task.objects.create(title=title)
+        return redirect("task_list")
+
+    tasks = Task.objects.all().order_by("-created_at")
+    return render(request, "tasks/task_list.html", {"tasks": tasks})
+
+def delete_task(request, id):
+    Task.objects.get(id=id).delete()
+    return redirect("task_list")
